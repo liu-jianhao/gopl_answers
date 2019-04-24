e3783a4f81ebfab59a3dc3cb791f6f596cfffa6a
liu-jianhao <jianhaoliu17@gmail.com>
2019-04-24 22:19:42 +0800

    ch1_solution

---
+++ ch1/1.4/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			counts = make(map[string]int)
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			countLines(f, counts)
+			for _, n := range counts {
+				if n > 1 {
+					fmt.Printf("%s\n", arg)
+					break
+				}
+			}
+			f.Close()
+		}
+	}
+
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+	// NOTE: ignoring potential errors from input.Err()
+}
---
+++ ch1/1.4/test.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+go build main.go
+./main dup1.txt dup2.txt
