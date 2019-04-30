282e694cc4e47e7f448bfbdaf29188690b3150a3
liu-jianhao <jianhaoliu17@gmail.com>
2019-04-30 21:52:41 +0800

    5.5

---
+++ ch5/5.5/countWordsAndImages.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Printf("countWordsAndImages: %s\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	words, images, err := CountWordsAndImages(os.Args[1])
+	if err != nil {
+		fmt.Printf("error in CountWordsAndImages\n")
+		os.Exit(1)
+	}
+	fmt.Printf("words = %d\nimages =  %d\n", words, images)
+}
+
+func CountWordsAndImages(url string) (words, images int, err error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return
+	}
+
+	doc, err := html.Parse(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		err = fmt.Errorf("parsing HTML: %s", err)
+		return
+	}
+	words, images = countWordsAndImages(doc)
+	return
+}
+
+func countWordsAndImages(n *html.Node) (words, images int) {
+	if n.Type == html.TextNode {
+		scanner := bufio.NewScanner(strings.NewReader(n.Data))
+		scanner.Split(bufio.ScanWords)
+		for scanner.Scan() {
+			words++
+		}
+	}
+
+	if n.Type == html.ElementNode && n.Data == "img" {
+		images++
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		s, i := countWordsAndImages(c)
+		words += s
+		images += i
+	}
+	return words, images
+}
---
+++ ch5/5.5/run.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+go build ./countWordsAndImages.go
+./countWordsAndImages http://www.baidu.com
